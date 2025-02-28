d262ba992d106a2c472abb8927c597e259ff8269
SalvadorMurillo <salvadormurillo612@gmail.com>
2025-02-28 03:46:09 -0600

    cambiar al puerto 3000

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Conectar a MongoDB
